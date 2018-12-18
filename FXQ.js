addac74a706bf56e9fbc074aa16cc005a7bfe308
聂志良 <1132263176@qq.com>
2018-12-18 09:06:29 +0800

    修改椭圆公章

--- FXQ.js
+++ FXQ.js
@@ -42,7 +42,6 @@ var FXQ = {
      */
     companySeal: function (company,cType,fType,sType,seaNo) {
 
-        console.log("sdfsdf")
         //获取canvans doc对象
         var canvas = document.getElementById('canvas');
 
@@ -258,113 +257,138 @@ var FXQ = {
         return canvas.toDataURL();
     },
     //椭圆签章
-    companyEllipse: function (bool,company,fType,cType) {
+    companyEllipse: function (company,serNo,fType,cType) {
+
+        //椭圆长轴半径
+        var radiusX = 140;
+        //短轴半径
+        var radiusY = 90;
         this.commonMethod(cType,fType);
         var color = FXQ.baseConf.color;
         var font = FXQ.baseConf.font;
 
-        var canvas = document.getElementById("canvas");
-        var  ctx=canvas.getContext('2d');
-        canvas.width = 270;
-        canvas.height = 250;
+        var canvas = document.getElementById('canvas');
 
-        var eRadiusX = 123;
-        var eRadiusY = 80;
+        canvas.width = 2 * radiusX + 5;
+        canvas.height = 2 * radiusY + 5;
 
-        //不多加一个字位置不对，这里临时处理一下
-        company += '哈';
+        var context = canvas.getContext('2d');
 
+        writeFont(true,company);
+        writeFont(false,serNo);
+        writeTitle();
+        drawEllipse();
 
-        //画椭圆
-        ctx.ellipse(135,125,132,eRadiusY,0,0,Math.PI*2);
-        ctx.fillStyle="rgba(255, 255, 255, 0)";
-        ctx.lineWidth=4
-        ctx.strokeStyle=color;
-        ctx.fill();
-        ctx.stroke();
+        function writeFont(isTop,words) {
 
-        ctx.translate(125,145)
-        var radiusX = 123;
-        var radiusY = 75;
-        var minRat = 0.2;
-        //总角度
-        var totalArcAng = 180;
-        var startAng =  bool == true ? -90 - totalArcAng/2 : 90 -totalArcAng/2;
-        console.log(startAng)
-        //从边线想中心移动的因子
-        var step = 0.5;
-        var count = company.length;
-        var allCount =  Math.ceil(totalArcAng / step) +1;
+            var totalArcAng = 180;
+            font = "20px "+font
 
-        var angArr = new Array(allCount);
-        var arcLenArr = new Array(allCount);
+            //字体长度
+            if (!isTop) {
+                totalArcAng = 60;
+                font = "10px "+font
+            }
+            var fontTextLen = words.length;
 
-        var num = 0;
-        var accArcLen = 0;
-        angArr[num] = startAng;
-        arcLenArr[num] = accArcLen;
-        num=num+1;
+            var radiusWidth = radiusX + context.lineWidth;
+            var radiusHeight = radiusY + context.lineWidth;
 
-        var angR = startAng * Math.PI / 180;
-        console.log(angR)
-        var lastX = radiusX * Math.cos(angR) +eRadiusX;
-        var lastY = radiusY * Math.sin(angR) +eRadiusY;
-
-        for (var i = startAng + step; num < allCount; i += step) {
-            angR = i * Math.PI / 180;
-
-            var x = radiusX * Math.cos(angR) + eRadiusX;
-            var y = radiusY * Math.sin(angR) + eRadiusY;
-            console.log("x:%s y: %s",x,y)
-            accArcLen += Math.sqrt((lastX-x) * (lastX-x) + (lastY - y)* (lastY-y));
-            angArr[num] = i;
+            //从边线向中心的移动因子
+            var minRat = 0.9;
+            //起始角度
+            var startAngle = isTop == true ? -90 -totalArcAng/2 : 90 - totalArcAng/2;
+            var step = 0.5;
+            var alCount = Math.ceil(totalArcAng / step) + 1;
+            var angleArr = new Array(alCount);
+            var arcLenArr = new Array(alCount);
+            var num = 0;
+            var accArcLen = 0;
+            angleArr[num] = startAngle;
             arcLenArr[num] = accArcLen;
-            lastX = x;
-            lastY = y;
-            num = num + 1;
-        }
+            num++;
+            var angR = startAngle * Math.PI / 180;
+            var lastX = radiusX * Math.cos(angR) + radiusWidth;
+            var lastY = radiusY * Math.sin(angR) + radiusHeight;
 
-        var arcPer = accArcLen / count;
-
-        for (var i = 0; i < count -1; i++ ) {
-            var arcL = i * arcPer + arcPer/2;
-            var ang = 0;
-
-            for (var p = 0; p < arcLenArr.length - 1; p++) {
-                if (arcLenArr[p] <= arcL && arcL <= arcLenArr[p + 1]) {
-                    ang = (arcL >= ((arcLenArr[p] + arcLenArr[p + 1]) /2 )) ? angArr[p + 1] : angArr[p];
-                    break;
-                }
+            for (var i = startAngle + step; num < alCount; i+=step) {
+                angR = i * Math.PI / 180;
+                var x = radiusX * Math.cos(angR) + radiusWidth;
+                var y = radiusY * Math.sin(angR) + radiusHeight;
+                accArcLen += Math.sqrt((lastX - x) * (lastX - x) + (lastY - y) * (lastY - y));
+                angleArr[num] = i;
+                arcLenArr[num] = accArcLen;
+                lastX = x;
+                lastY = y;
+                num++;
             }
 
-            angR = (ang * Math.PI / 180);
-            var x = radiusX * Math.cos(angR);
-            var y = radiusY * Math.sin(angR);
-            var qxang = Math.atan2(radiusY * Math.cos(angR),-radiusX * Math.sin(angR));
-            var fxang = qxang + Math.PI / 2;
-            var c = company[i]
+            var arcPer = accArcLen / fontTextLen;
+            for (var i = 0; i < fontTextLen; i++) {
+                var arcL = i * arcPer + arcPer / 2;
+                var ang = 0;
 
-            var w = 20;
-            var h = 20;
+                for (var p = 0; p < arcLenArr.length - 1; p++) {
+                    if (arcLenArr[p] <= arcL && arcL <= arcLenArr[p + 1]) {
+                        ang = (arcL >= ((arcLenArr[p] + arcLenArr[p + 1]) /2)) ? angleArr[p + 1] : angleArr[p];
+                        break;
+                    }
+                }
+                angR = (ang * Math.PI / 180);
+                var x = radiusX * Math.cos(angR) + radiusX;
+                var y = radiusY * Math.sin(angR) + radiusY;
+                var qxang = Math.atan2(radiusY * Math.cos(angR),-radiusX * Math.sin(angR));
+                var fxang = qxang + Math.PI / 2;
 
-            x = x + h * minRat * Math.cos(fxang);
-            y = y + h * minRat * Math.sin(fxang);
+                var subIndex = isTop == true ? i : fontTextLen - 1 -i;
+                var c = words[subIndex];
+                var w = 25; var h = 31;
 
-            x = x + w/2 * Math.cos(qxang);
-            y = y + w/2 * Math.sin(qxang)
+                if (!isTop) {
+                    w = 2; h = 10;
+                }
 
-            console.log("c:%s x:%s y:%s",c,x,y)
-            ctx.fillStyle = color;
-            ctx.font = '20px '+ font;
-            ctx.fillText(c,x,y);
-            ctx.restore();
+                x += (h * minRat) * Math.cos(fxang);
+                y += (h * minRat) * Math.sin(fxang);
+                if (isTop) {
+                    x += -w / 2 * Math.cos(qxang);
+                    y += -w / 2 * Math.sin(qxang);
+                } else {
+                    x += w / 2 * Math.cos(qxang);
+                    y += w / 2 * Math.sin(qxang);
+                }
 
-           // ctx.textBaseline = 'middle';//设置文本的垂直对齐方式
-            //ctx.textAlign = 'center'; //设置文本的水平对对齐方式
-            ctx.lineWidth=1;
-            ctx.fillStyle = FXQ.baseConf.color;
-            ctx.font = '20px '+ font;
-            ctx.fillText('某某专用章',-35,20)
+                context.save()
+                context.translate(x,y);
+                if (isTop == true) {
+                    context.rotate((fxang * 180 / Math.PI - 90) * Math.PI / 180)
+                } else {
+                    context.rotate((fxang * 180 / Math.PI + 180 -90)* Math.PI / 180)
+                }
+                context.translate(-x,-y)
+                context.fillStyle = color;
+                context.font = font;
+                context.fillText(c,x,y);
+                context.restore();
+            }
+        }
+
+        function writeTitle() {
+            context.fillStyle = color;
+            context.font = '20px SimSun';
+            context.textAlign = 'center';
+            context.fillText('合同专用章',radiusX,radiusY+20);
+            context.restore();
+        }
+
+        function drawEllipse() {
+            context.ellipse(radiusX + context.lineWidth+1,radiusY + context.lineWidth+1,radiusX,radiusY,0,0,Math.PI*2);
+            //背景透明
+            context.fillStyle = "rgba(255, 255, 255, 0)";
+            context.strokeStyle = color;
+            context.lineWidth = 3;
+            context.fill();
+            context.stroke();
         }
     },
     /**
